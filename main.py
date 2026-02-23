bceac6ed1fd7e8e4eb86747dafe4856be9cff6c4
gleb <vandal2006a@gmail.com>
2026-02-23 15:27:42 +0300

    log aiogram

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import asyncio
 from app.bot.bot import start_telegram_bot
+from app.core.logger import get_logger
+
+get_logger('aiogram')
 
 if __name__ == "__main__":
     asyncio.run(start_telegram_bot())
