f2818280d34916e6cd0a334e650f6080ce837ba9
gleb <vandal2006a@gmail.com>
2026-02-23 14:01:15 +0300

    Refactor bot initialization to remove AiohttpSession and simplify session handling; update docker-compose to ensure proper DNS and sysctl configurations

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,8 +1,5 @@
 import asyncio
-import socket
 from aiogram import Bot, Dispatcher
-from aiogram.client.session.aiohttp import AiohttpSession
-from aiohttp import ClientTimeout, TCPConnector
 from app.bot.router import router
 from app.core.config import settings
 from app.core.logger import get_logger
@@ -30,13 +27,7 @@ async def keep_alive(bot: Bot):
 
 async def start_telegram_bot():
     try:
-        connector = TCPConnector(family=socket.AF_INET)  # только IPv4
-        session = AiohttpSession(
-            connector=connector,
-            timeout=ClientTimeout(total=300),
-        )
-
-        bot = Bot(token=settings.BOT_TOKEN, session=session)
+        bot = Bot(token=settings.BOT_TOKEN)
         dp = Dispatcher()
 
         # dp.message.middleware(OnlyGroupMiddleware())
